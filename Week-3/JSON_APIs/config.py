d806305382aa4f85fedd3e3f6a9d41c7ae192398
“Garrett <44751888+garrettstevenkeyes@users.noreply.github.com>
2019-10-28 11:08:44 -0400

    commit26

---
+++ Week-3/JSON_APIs/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 07:54:55 2019
+
+@author: flatironschool
+"""
+yelpkey = 'ZIGcyQZxe5MMe1ivQHMW8ekUFSARzrT1kFhsyjJedKXsmPqWzoj-oulmza8b11laZLzEI1xQQk_EBI5JGqGZsZBY6Yub5Fm25djGgPWXN5yzIrFwWrUERmz-t9uxXXYx'
+password = 'flatiron'
+host = 'flatiron.cr2tqw6vxlai.us-east-2.rds.amazonaws.com'
+user = 'admin'
---
+++ Week-3/SQL/SQL_Practice/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 15:10:58 2019
+
+@author: flatironschool
+"""
+password = 'flatiron'
+host = 'flatiron.cr2tqw6vxlai.us-east-2.rds.amazonaws.com'
+user = 'admin'
